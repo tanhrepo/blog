4d8f687bebf9b1226bab41b2551587b034a11961
tanhong <tanhong@asiainfo.com>
2021-06-21 09:37:46 +0800

    js-init

---
+++ project/JavaScript/JSDemo1.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: tanhong
+ * @Date: 2021-06-15 10:52:39
+ * @LastEditTime: 2021-06-15 11:28:19
+ * @LastEditors: tanhong
+ * @FilePath: \project\JavaScript\JSDemo1.js
+ */
+
+// 完整的js包括ECMAScript（核心）、DOM（文档对象模型）、BOM（浏览器对象模型）
+
+// Array.prototype.
+// flat()/flatMap()、
+// String.prototype.trimStart()/trimEnd()、
+// Object.fromEntries()方法，
+// 以及Symbol.prototype.description属性，
+// 明确定义了Function.prototype.toString()
+// 的返回值并固定了Array.prototype.sort()的顺序。
+
+// BOM主要针对浏览器窗口和子窗口frame。
+
+html中的script
