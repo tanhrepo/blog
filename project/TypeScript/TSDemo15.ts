fefa6742dcf2a62af35884dc8829e22c00696c09
tanhong <tanhong@asiainfo.com>
2021-07-10 11:50:41 +0800

    。。

--- project/TypeScript/TSDemo15.ts
+++ project/TypeScript/TSDemo15.ts
@@ -1,8 +1,8 @@
-/*    “等下进去后，不要离开我太远，就在我五米范围内活动，一旦你感觉到危险，马上示警，到时候我们就沿着进去的路往后撤退，听的懂吗？”*/
+/*    升级方向一：采用石质加固，增加排水效率，增加耐用性，且不易被腐蚀，需要生存点（147）*/
 /*
  * @Author: tanhong
  * @Date: 2021-05-28 16:49:22
- * @LastEditTime: 2021-07-10 11:13:30
+ * @LastEditTime: 2021-07-10 11:50:32
  * @LastEditors: tanhong
  * @FilePath: \blog\project\TypeScript\TSDemo15.ts
  */
