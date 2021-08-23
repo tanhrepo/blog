4c4167032b8433aa6b6bbdd97fef744d4a4f6181
tanhong <tanhong@asiainfo.com>
2021-08-23 16:15:50 +0800

    前端

---
+++ note/ES6-ES11/let和const/let.md
@@ -0,0 +1,16 @@
+# let
+
+ 声明变量
+
+~~~js
+      let a;
+      let b, c, d;
+~~~
+
+#### 特性1：变量不能重复声明
+
+~~~js
+	  let star = 'qingfeng';
+      let star = 'mingzhu';
+~~~
+
---
+++ project/JavaScript/异步.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: tanhong
+ * @Date: 2021-08-23 16:15:12
+ * @LastEditTime: 2021-08-23 16:15:30
+ * @LastEditors: tanhong
+ * @FilePath: \blog\project\JavaScript\异步.js
+ */
+// js是单线程
