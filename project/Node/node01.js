eeb5abecc3723d40cd1783793a74452c73fb3f4b
tanhong <tanhong@asiainfo.com>
2021-09-01 10:44:44 +0800

    node

---
+++ project/Node/node01.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: tanhong
+ * @Date: 2021-08-31 11:36:10
+ * @LastEditTime: 2021-09-01 10:44:43
+ * @LastEditors: tanhong
+ * @FilePath: \blog\project\Node\node01.js
+ */
+console.log("hello,world!");
+
+let a = 3,
+  b = 4;
+console.log(a + b);
