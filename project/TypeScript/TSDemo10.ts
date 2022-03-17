801d79c8c713dbc9b3bded73ee2f571220463b2d
tanhong <tanhongmail@foxmail.com>
2022-03-17 11:26:00 +0800

    rtwret

--- project/TypeScript/TSDemo10.ts
+++ project/TypeScript/TSDemo10.ts
@@ -7,6 +7,11 @@
  */
 
 // 类
+
+
+// 类的基本使用
+
+// 定义一个最简单的Lady类,这里要使用关键字class,类里边有姓名属性和一个得到姓名的方法：
 // class Lady {
 //   content: string = 'hi,handsome boy';
 //   say() {
@@ -14,7 +19,16 @@
 //   }
 // }
 
-// // 类的继承
+// const goddess = new Lady();
+// console.log(goddess.say());
+
+
+
+
+// 类的继承
+
+// TypeScrip 的继承和ES6中的继承是一样的。关键字也是extends,比如我们这里新建一个XiaoJieJie的类，
+// 然后继承自Lady类，在XiaoJieJie类里写一个新的方法，叫做sayLove,具体代码如下。
 // class fang extends Lady {
 //   sayLove() {
 //     return 'Titanic';
