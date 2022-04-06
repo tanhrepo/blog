a7f233cfdb19286ec581ac17562a8411e119a135
tanhong <tanhongmail@foxmail.com>
2022-04-06 15:42:08 +0800

    master

--- project/TypeScript/TSDemo16.ts
+++ project/TypeScript/TSDemo16.ts
@@ -1,7 +1,7 @@
 // 正则匹配中国大陆手机号码
-function isChinaPhoneNumber(phoneNumber: string) : boolean {
-    var pattern = /^1[34578]\d{9}$/;
-    return pattern.test(phoneNumber);
-}
+// function isChinaPhoneNumber(phoneNumber: string) : boolean {
+//     var pattern = /^1[34578]\d{9}$/;
+//     return pattern.test(phoneNumber);
+// }
 
-isChinaPhoneNumber('12345678901');
+// console.log(isChinaPhoneNumber('34365465444'));
