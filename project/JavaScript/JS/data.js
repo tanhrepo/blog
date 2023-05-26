90c2f49675a191c96cdeafb94eff7b78fa017fa7
tanhong <tanhongmail@foxmail.com>
2023-05-26 09:15:03 +0800

    feat: blog

---
+++ project/JavaScript/JS/data.js
@@ -0,0 +1,11 @@
+const tmp = [
+  { id: 'P1', name: '温度传感器', dre: '沈阳市浑南区浑南二路', state: '已告警' },
+  { id: 'P2', name: '温度传感器', dre: '沈阳市和平区南三好街东', state: '已告警' },
+  { id: 'P3', name: '温度传感器', dre: '沈阳市浑南区浑南新区绮霞街6号', state: '已告警' },
+  { id: 'P4', name: '温度传感器', dre: '沈阳市和平区南湖街道澳门路10-5新世界花园', state: '已告警' },
+  { id: 'SDXQ1', name: '电表', dre: '沈阳市浑南区金阳大街', state: '已告警' },
+  { id: 'SDXQ2', name: '电表', dre: '沈阳市沈河区文艺路33号', state: '已告警' },
+  { id: 'SDXQ3', name: '电表', dre: '沈阳市浑南区天赐街7-3', state: '已处理' }
+];
+let a = JSON.stringify(tmp);
+console.log(a);
