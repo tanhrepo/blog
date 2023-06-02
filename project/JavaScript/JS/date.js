0839d9288080f9e290a7de7791609ce48e54cc2b
tanhong <tanhongmail@foxmail.com>
2023-06-02 18:05:00 +0800

    新文件

---
+++ project/JavaScript/JS/date.js
@@ -0,0 +1,29 @@
+// 获取过去n天的日期，并且格式化，格式化字符串为入参，如yyyy-MM-dd HH:mm:ss 星期w
+// 2017-12-12 12:12:12 星期二
+function getPastDate(n, format) {
+    var date = new Date();
+    date.setDate(date.getDate() - n);
+    return formatDate(date, format);
+}
+
+function formatDate(date, format) {
+    var o = {
+        "M+": date.getMonth() + 1, //month
+        "d+": date.getDate(), //day
+        "H+": date.getHours(), //hour
+        "m+": date.getMinutes(), //minute
+        "s+": date.getSeconds(), //second
+        "w": date.getDay(), //week
+        "q+": Math.floor((date.getMonth() + 3) / 3), //quarter
+        "S": date.getMilliseconds() //millisecond
+    }
+    if (/(y+)/.test(format)) format = format.replace(RegExp.$1,
+        (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(format))
+            format = format.replace(RegExp.$1,
+                RegExp.$1.length == 1 ? o[k] :
+                ("00" + o[k]).substr(("" + o[k]).length));
+    return format;
+}
+
