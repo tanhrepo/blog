3e97856a73cd70a543363e4c8a08ab3f4177c560
tanhong <tanhongmail@foxmail.com>
2023-10-19 19:05:40 +0800

    迁移笔记

--- note/Vuex.md
+++
@@ -1,4 +0,0 @@
-#  Vuex
-
-![image-20201116125600105](https://raw.githubusercontent.com/tanhrepo/img/master/img/image-20201116125600105.png)
-
--- project/JavaScript/JS/jj.js
+++ project/JavaScript/JS/jj.js
@@ -1,4 +1,1 @@
-let date1 = 20220822
-// 将date1的yyyyMMdd转换为yyyy-MM-dd
-date1 = date1.toString().substring(0, 4) + '-' + date1.toString().substring(4, 6) + '-' + date1.toString().substring(6, 8)
-console.log(date1)
+// what nationality are you?
